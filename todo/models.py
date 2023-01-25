70d5590acb32c839334e7a2dc8f39920804423ee
mir9qub <nsalamat18@gmail.com>
2023-01-25 12:05:35 +0600

    first commit

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from todo.database.base import Base, engine
+from sqlalchemy import Column, String, Integer, Boolean
+
+
+
+class ToDo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    is_complete = Column(Boolean, default=False)
+
+
+Base.metadata.create_all(bind=engine)
